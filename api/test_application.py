3e3a7192963310902c86a0159292e3da458f94fc
jocosiol <jocosiol@gmail.com>
2022-02-17 20:02:28 +0200

    more testing

--- api/test_application.py
+++ api/test_application.py
@@ -1,6 +1,7 @@
 import unittest
 import requests
 import application
+import json
 
 class TestApplication(unittest.TestCase):
     #URLS
@@ -10,18 +11,35 @@ class TestApplication(unittest.TestCase):
 
     #OBJECTS
     NEW_ACCOUNT_OBJ = {
-    "name": "James B",
-    "document": "007",
+    "name": "James C",
+    "document": "008",
     "dailyWithdrawLimit": "500",
     "accountType": "100"
-}
+    }
+    NEW_ACCOUNT_ID = ""
+
 
     def test_1_get_accounts(self):
         r = requests.get(TestApplication.ACCOUNT_URL)
+        res = r.content
+        print(res)
         self.assertEqual(r.status_code, 200)
 
+    @unittest.skip("Skip this.")
     def test_2_create_new_account(self):
         r = requests.post(TestApplication.CREATE_ACCOUNT_URL, json=TestApplication.NEW_ACCOUNT_OBJ)
+        data = json.loads(r.content)
+        TestApplication.NEW_ACCOUNT_ID = data['accountId']
         self.assertEqual(r.status_code, 200)
-    
-    
+
+ 
+    NEW_DEPOSIT = {
+    "accountId": 14,
+    "value": 1000
+    }
+
+    def test_3_deposit(self):
+        r = requests.post("{}/account/deposit".format(TestApplication.API_URL), json=TestApplication.NEW_DEPOSIT)
+        data = json.loads(r.content)
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(data['value'], 1000)
