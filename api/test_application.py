f3d2e72028bbf738e80146b7672c5f34c7f8f29e
jocosiol <jocosiol@gmail.com>
2022-02-17 09:39:50 +0200

    commit first test

--- api/test_application.py
+++ api/test_application.py
@@ -1,2 +1,11 @@
 import unittest
-from application import createNewAccount
+import requests
+import application
+
+class TestApplication(unittest.TestCase):
+    API_URL = "http://127.0.0.1:5000"
+    BALANCE_URL = "{}/account".format(API_URL)
+
+    def test_1_get_accounts(self):
+        r = requests.get(TestApplication.BALANCE_URL)
+        self.assertEqual(r.status_code, 200)
