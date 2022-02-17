71e67ce71bcc64419957808a7b83d0ba42751580
jocosiol <jocosiol@gmail.com>
2022-02-17 09:57:37 +0200

    Commit more testing

--- api/test_application.py
+++ api/test_application.py
@@ -3,9 +3,25 @@ import requests
 import application
 
 class TestApplication(unittest.TestCase):
+    #URLS
     API_URL = "http://127.0.0.1:5000"
-    BALANCE_URL = "{}/account".format(API_URL)
+    ACCOUNT_URL = "{}/account".format(API_URL)
+    CREATE_ACCOUNT_URL = "{}/account/create".format(API_URL)
+
+    #OBJECTS
+    NEW_ACCOUNT_OBJ = {
+    "name": "James B",
+    "document": "007",
+    "dailyWithdrawLimit": "500",
+    "accountType": "100"
+}
 
     def test_1_get_accounts(self):
-        r = requests.get(TestApplication.BALANCE_URL)
+        r = requests.get(TestApplication.ACCOUNT_URL)
         self.assertEqual(r.status_code, 200)
+
+    def test_2_create_new_account(self):
+        r = requests.post(TestApplication.CREATE_ACCOUNT_URL, json=TestApplication.NEW_ACCOUNT_OBJ)
+        self.assertEqual(r.status_code, 200)
+    
+    
